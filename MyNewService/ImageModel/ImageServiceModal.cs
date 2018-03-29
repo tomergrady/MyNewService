bea1260cf0c1358fa1125c8b45ae1f1f2dba08a5
raz Shenkman <shenkmanraz@gmail.com>
2018-03-29 21:15:47 +0300

    Part C

--- MyNewService/ImageModel/ImageServiceModal.cs
+++ MyNewService/ImageModel/ImageServiceModal.cs
@@ -15,17 +15,38 @@ namespace ImageService.Modal
     public class ImageServiceModal : IImageServiceModal
     {
         #region Members
-        private string m_OutputFolder { get; set; }            // The Output Folder
-        private int m_thumbnailSize { get; set; }              // The Size Of The Thumbnail Size
-
+        public string m_OutputFolder { get; set; }            // The Output Folder
+        public int m_thumbnailSize { get; set; }              // The Size Of The Thumbnail Size
         public string AddFile(string path, out bool result)
         {
-            //open a file
-            result = true;
-            return "";
+            try
+            {
+                //System.IO.File.Copy(path, m_OutputFolder, true);
+                DateTime inputFileTime = File.GetCreationTime(path);
+                String year = inputFileTime.Year.ToString();
+                String month = inputFileTime.Month.ToString();
+                Directory.CreateDirectory(m_OutputFolder);
+                Directory.CreateDirectory(m_OutputFolder + "\\" + year);
+                Directory.CreateDirectory(m_OutputFolder + "\\" + year + "\\" + month);
+                Directory.CreateDirectory(m_OutputFolder + "\\" + "Thumbnails");
+                Directory.CreateDirectory(m_OutputFolder + "\\" + "Thumbnails" + "\\" + year);
+                Directory.CreateDirectory(m_OutputFolder + "\\" + "Thumbnails" + "\\" + year + "\\" + month);
+                string finalOutputPath = m_OutputFolder + "\\" + year + "\\" + month + "\\";
+                string finalOutputTPath = m_OutputFolder + "\\" + "Thumbnails" + year + "\\" + month + "\\";
+                Image thumbnail = Image.FromFile(path);
+                thumbnail = (Image)(new Bitmap(thumbnail, new Size(this.m_thumbnailSize, this.m_thumbnailSize)));
+                thumbnail.Save(finalOutputTPath);
+                File.Move(path, finalOutputPath);
+                result = true;
+                return "The file: " + Path.GetFileName(path) + " is now added to " + finalOutputPath +
+                    " and also to the thumbnails folder";
+            } catch (Exception ex)
+            {
+                result = false;
+                return ex.ToString();
+            }
+
         }
-
         #endregion
-
     }
 }
--- MyNewService/Logging/LoggingService.cs
+++ MyNewService/Logging/LoggingService.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ImageService.Modal;
 
 namespace ImageService.Logging
 {
@@ -14,7 +15,8 @@ namespace ImageService.Logging
         public void Log(string message, MessageTypeEnum type)
         {
             MessageRecieved?.Invoke(this, new MessageRecievedEventArgs(type, message));
-
+            ImageServiceModal gfaf = new ImageServiceModal();
+                        
         }
     }
 }
