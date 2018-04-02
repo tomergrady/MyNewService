a83faeb9592373a14474244b5e12c744e6ac2142
raz Shenkman <shenkmanraz@gmail.com>
2018-04-02 18:09:43 +0300

    part d

--- MyNewService/ImageService/Server/ImagerServer.cs
+++ MyNewService/ImageService/Server/ImagerServer.cs
@@ -1,6 +1,5 @@
 ﻿using ImageService.Controller;
 using ImageService.Controller.Handlers;
-//using ImageService.Infrastructure.Enums;
 using ImageService.Logging;
 using ImageService.Modal;
 using System;
@@ -8,6 +7,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ImageService.Logging.Modal;
+using ImageService.Infrastructure.Enums;
 
 namespace ImageService.Server
 {
@@ -31,18 +32,38 @@ namespace ImageService.Server
                 // create handler for each path
                 directoryHandlers[i] = new DirectoyHandler(m_controller, m_logging, paths[i]);
                 this.CommandRecieved += directoryHandlers[i].OnCommandRecieved;
-
+                directoryHandlers[i].DirectoryClose += this.RemoveDirectoryHandler;
             }
         }
 
         public void CloseServer()
         {
-
+            this.m_logging.Log("starting closing server", MessageTypeEnum.INFO);
+            CommandRecievedEventArgs commandRecievedEventArgs = new CommandRecievedEventArgs((int)CommandEnum.CloseCommand, null, "");
+            this.CommandRecieved?.Invoke(this, commandRecievedEventArgs);
         }
 
         public void CloseAllHandlers()
         {
 
+        }
+
+        private void RemoveDirectoryHandler(object sender, DirectoryCloseEventArgs messageArgs)
+        {
+            IDirectoryHandler sendingDirectoryHandler = sender as IDirectoryHandler;
+            if (sendingDirectoryHandler == null)
+            {
+                //an object that isn't supposed to activate the event did it
+                this.m_logging.Log("unotherized object tried to announce about closing of directory in path: " + messageArgs.DirectoryPath, MessageTypeEnum.WARNING);
+                return;
+            }
+            this.m_logging.Log("Directory Handler of Directory in: " + messageArgs.DirectoryPath + @" with message: " + messageArgs.Message, MessageTypeEnum.INFO);
+            //unsubscribing of the DirectoryHandler from the server message feed
+            this.CommandRecieved -= sendingDirectoryHandler.OnCommandRecieved;
+            if (this.CommandRecieved == null) {
+                //if all the Directory Handlers closed succefully the server itself can finally close
+                 this.m_logging.Log("closing server finally", MessageTypeEnum.INFO);
+            }
         }
 
     }
